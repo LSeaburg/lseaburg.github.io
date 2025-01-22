42bead51f4614eeb51a8c557f97684e7fc76030d
Logan Seaburg <logan.seaburg@gmail.com>
2025-01-21 19:30:37 -0500

    Update footer with links

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -15,11 +15,32 @@ function ArrowIcon() {
   )
 }
 
+function FooterLink(props) {
+  return (
+    <li>
+      <a
+        className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+        rel="noopener noreferrer"
+        target="_blank"
+        href={props.link}
+      >
+        <ArrowIcon />
+        <p className="ml-2 h-7">{props.text}</p>
+      </a>
+    </li>
+  )
+}
+
 export default function Footer() {
   return (
     <footer className="mb-16">
+      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+        <FooterLink text="github" link="https://github.com/LSeaburg"/>
+        <FooterLink text="linkedin" link="https://www.linkedin.com/in/logan-seaburg-5126ab147/"/>
+        <FooterLink text="instagram" link="https://www.instagram.com/loganrseaburg/"/>
+      </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+        © {new Date().getFullYear()} Logan Seaburg
       </p>
     </footer>
   )
